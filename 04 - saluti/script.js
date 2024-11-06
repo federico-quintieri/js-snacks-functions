425490f60daa9db1a4142ad3668e4ea8267e5fd9
federico-quintieri <99342330+fede-quinty@users.noreply.github.com>
2024-11-06 14:52:24 +0100

    feat:fourth exercise completed

--- 04 - saluti/script.js
+++ 04 - saluti/script.js
@@ -1,13 +1,18 @@
 /* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
 
-const name = 'Mario';
-
+const name = "Mario";
 
 // Dichiara la funzione qui.
+/**
+ * Description
+ * @param {string} name
+ * @returns {string}
+ */
 
+const helloFunction = (name) => `Ciao ${name}`;
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+const saluto = helloFunction(name);
+console.log(saluto);
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
