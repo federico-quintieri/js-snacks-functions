3ed7f25aa21ded1e211ca9b1c9d96390f6b2defc
federico-quintieri <99342330+fede-quinty@users.noreply.github.com>
2024-11-06 14:25:23 +0100

    feat: first exercise completed

--- 01 - Iniziali/script.js
+++ 01 - Iniziali/script.js
@@ -2,12 +2,25 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
-
 // Dichiara la funzione qui.
+const inizialiArray = (array) => {
+  let returnArray = [];
 
+  // Ciclo che itera ogni elemento dell'array
+  for (let i = 0; i < array.length; i++) {
+    // Salviamo l'elemento i dell'array
+    let currElement = array[i];
+    // Prendiamo l'indice 0 della stringa elemento
+    let inizialeElement = currElement[0];
+    // Pushiamo questo carattere nell'array da ritornare
+    returnArray.push(inizialeElement);
+  }
+  // Ritorno nuovo array con solo iniziali di elementi del precedente array
+  return returnArray;
+};
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
-
+// Passiamo l'array names come parametro d'ingresso
+const returnArray = inizialiArray(names);
+console.log(returnArray);
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
