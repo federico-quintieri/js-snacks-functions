b13322b66f7587046ef1cb7344763bc81686b659
federico-quintieri <99342330+fede-quinty@users.noreply.github.com>
2024-11-06 15:00:27 +0100

    feat:bonus exercise completed

--- 05 - bonus/script.js
+++ 05 - bonus/script.js
@@ -5,14 +5,36 @@ buon pomeriggio se è pomeriggio (fino alle 17)
 e buonasera se è sera (oltre le 17)
 */
 
-const name = 'Mario';
-
+const name = "Mario";
 
 // Dichiara la funzione qui.
+/**
+ * Description
+ * @param {string} name
+ * @returns {string}
+ */
 
+const helloFunction = (name) => {
+  // Variabile contenente la stringa di ritorno
+  let saluto = "";
+
+  // Variabile contenente istanza Date()
+  const time = new Date();
+
+  // Variabile contenente ora presa tramite metodo getHours
+  const ora = time.getHours();
+
+  // Serie di if per confrontare l'orario e cambiare di conseguenza la stringa di return
+  if (ora >= 0 && ora < 13) saluto = `Buongiorno ${name}`;
+  else if (ora >= 13 && ora < 17) saluto = `Buon Pomeriggio ${name}`;
+  else if (ora >= 17 && ora <= 23) saluto = `Buonasera ${name}`;
+
+  // Ritorniamo la stringa modificata.
+  return saluto;
+};
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+const saluto = helloFunction(name);
+console.log(saluto);
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
